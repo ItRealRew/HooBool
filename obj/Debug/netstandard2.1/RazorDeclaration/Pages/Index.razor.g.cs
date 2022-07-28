3cc141e7dceca7c2764598d5d4a25ef897986c10
ItRealRew <rew-proninmax@yandex.ru>
2022-07-28 11:38:21 +0300

    Тест

--- obj/Debug/netstandard2.1/RazorDeclaration/Pages/Index.razor.g.cs
+++ obj/Debug/netstandard2.1/RazorDeclaration/Pages/Index.razor.g.cs
@@ -11,112 +11,112 @@ namespace WebAssemblyBlazorChromeExtension.Pages
     using System.Linq;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "c:\Users\User\Desktop\HoodBool\_Imports.razor"
+#line 1 "C:\Users\User\Desktop\HoodBool\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "c:\Users\User\Desktop\HoodBool\_Imports.razor"
+#line 2 "C:\Users\User\Desktop\HoodBool\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "c:\Users\User\Desktop\HoodBool\_Imports.razor"
+#line 3 "C:\Users\User\Desktop\HoodBool\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "c:\Users\User\Desktop\HoodBool\_Imports.razor"
+#line 4 "C:\Users\User\Desktop\HoodBool\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "c:\Users\User\Desktop\HoodBool\_Imports.razor"
+#line 5 "C:\Users\User\Desktop\HoodBool\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "c:\Users\User\Desktop\HoodBool\_Imports.razor"
+#line 6 "C:\Users\User\Desktop\HoodBool\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "c:\Users\User\Desktop\HoodBool\_Imports.razor"
+#line 7 "C:\Users\User\Desktop\HoodBool\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "c:\Users\User\Desktop\HoodBool\_Imports.razor"
+#line 8 "C:\Users\User\Desktop\HoodBool\_Imports.razor"
 using WebAssemblyBlazorChromeExtension;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "c:\Users\User\Desktop\HoodBool\_Imports.razor"
+#line 9 "C:\Users\User\Desktop\HoodBool\_Imports.razor"
 using WebAssemblyBlazorChromeExtension.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "c:\Users\User\Desktop\HoodBool\_Imports.razor"
+#line 10 "C:\Users\User\Desktop\HoodBool\_Imports.razor"
 using System.Threading;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "c:\Users\User\Desktop\HoodBool\_Imports.razor"
+#line 11 "C:\Users\User\Desktop\HoodBool\_Imports.razor"
 using System;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "c:\Users\User\Desktop\HoodBool\_Imports.razor"
+#line 12 "C:\Users\User\Desktop\HoodBool\_Imports.razor"
 using System.Diagnostics;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 13 "c:\Users\User\Desktop\HoodBool\_Imports.razor"
+#line 13 "C:\Users\User\Desktop\HoodBool\_Imports.razor"
 using System.IO;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 14 "c:\Users\User\Desktop\HoodBool\_Imports.razor"
+#line 14 "C:\Users\User\Desktop\HoodBool\_Imports.razor"
 using System.Threading.Tasks;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 15 "c:\Users\User\Desktop\HoodBool\_Imports.razor"
+#line 15 "C:\Users\User\Desktop\HoodBool\_Imports.razor"
 using System.Text.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 16 "c:\Users\User\Desktop\HoodBool\_Imports.razor"
+#line 16 "C:\Users\User\Desktop\HoodBool\_Imports.razor"
 using System.Text.Json.Serialization;
 
 #line default
@@ -131,7 +131,7 @@ using System.Text.Json.Serialization;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 21 "c:\Users\User\Desktop\HoodBool\Pages\Index.razor"
+#line 21 "C:\Users\User\Desktop\HoodBool\Pages\Index.razor"
        
     // Количество потраченого времени.
     public string seconds = "";
